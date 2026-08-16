cd3d8ea8c2c1a858dffaae4045ec4d0f58c916df
Tifa710 <tefatarek1@gmail.com>
2026-08-16 23:12:34 +0300

    feat: add routing and layout components for authentication and main features
    - Implemented routing for login, register, forget password, feed, profile, notification, and change password components.
    - Created AuthLayoutComponent and MainLayoutComponent to manage different layouts.
    - Added respective components for login, register, forget password, feed, profile, notification, change password, and not found pages.
    - Updated index.html to include Google Fonts.
    - Configured Tailwind CSS and Flowbite in styles.css and .postcssrc.json.
    - Added basic styles and HTML structure for new components.
    - Created unit tests for all new components to ensure functionality.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import {
+  provideRouter,
+  withHashLocation,
+  withInMemoryScrolling,
+  withViewTransitions,
+} from '@angular/router';
 import { routes } from './app.routes';
-
+import { provideHttpClient, withFetch } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(
+      routes,
+      withHashLocation(),
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
+      withViewTransitions({ skipInitialTransition: true }),
+    ),
+    provideHttpClient(withFetch()),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,63 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './Layout/auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './Layout/main-layout/main-layout.component';
+import { LoginComponent } from './Features/login/login.component';
+import { RegisterComponent } from './Features/register/register.component';
+import { ForgetPasswordComponent } from './Features/forget-password/forget-password.component';
+import { FeedComponent } from './Features/feed/feed.component';
+import { ProfileComponent } from './Features/profile/profile.component';
+import { NotificationComponent } from './Features/notification/notification.component';
+import { ChangePasswordComponent } from './Features/change-password/change-password.component';
+import { NotfoundComponent } from './Features/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      {
+        path: 'forgetPassword',
+        component: ForgetPasswordComponent,
+      },
+    ],
+  },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'feed',
+        component: FeedComponent,
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      },
+      {
+        path: 'notification',
+        component: NotificationComponent,
+      },
+      {
+        path: 'changePassword',
+        component: ChangePasswordComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: NotfoundComponent,
+  },
+];
