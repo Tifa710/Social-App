498a069f3cd5c2747f1135efcbdbaa4ca7ebe61f
Tifa710 <tefatarek1@gmail.com>
2026-08-19 00:06:51 +0300

    feat: enhance registration form with validation messages and improved layout

--- src/app/Features/register/register.component.ts
+++ src/app/Features/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   imports: [ReactiveFormsModule],
@@ -7,20 +8,28 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  registerForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    username: new FormControl(''),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    dateOfBirth: new FormControl('', [Validators.required]),
-    gender: new FormControl('', [Validators.required]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),
-    ]),
-    rePassword: new FormControl('', [Validators.required]),
-  });
+  private router = inject(Router);
+  registerForm = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      username: new FormControl(''),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      dateOfBirth: new FormControl('', [Validators.required]),
+      gender: new FormControl('', [Validators.required]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),
+      ]),
+      rePassword: new FormControl('', [Validators.required]),
+    },
+    { updateOn: 'submit' },
+  );
 
   onSubmit() {
-    console.log(this.registerForm);
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
   }
 }
